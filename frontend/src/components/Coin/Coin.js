cb4f612bb0d18a604ea75f7a4984a4af1d1e045a
JmJ0922 <jainmariajustine@gmail.com>
2024-03-05 17:56:59 +0530

    coin

--- frontend/src/components/Coin/Coin.js
+++ frontend/src/components/Coin/Coin.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import './Coin.css'
+import React from 'react';
+import './Coin.css';
+import image from './image.svg';
 
 const Coin = () => {
   return (
     <div className='coin'>
       <div className='items'>
-
-        <p>Bitcoin <span>BTC</span></p>
-
+        <div className='head'>
+          <div className='logo'>
+            <img src={image} alt="Image" width="36px" height="36px"/>
+          </div>
+          <div className='details'>
+            <h3>Bitcoin <span>BTC</span></h3>
+            <div className='rank'><span>Rank #1</span></div>
+          </div>
+        </div>
       </div>
-      <div>Chart</div>
+      <div className='chart'>Chart</div>
     </div>
-  )
-}
+  );
+};
 
-export default Coin
+export default Coin;
