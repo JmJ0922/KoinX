c6f2f2538f4f915f895c48dff415b4de2f13188c
JmJ0922 <jainmariajustine@gmail.com>
2024-03-05 20:59:36 +0530

    coin

--- frontend/src/components/Coin/Coin.js
+++ frontend/src/components/Coin/Coin.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Coin.css';
 import image from './image.svg';
+import TradingViewWidget from 'react-tradingview-widget';
+
 
 const Coin = () => {
+  const [bitcoinData, setBitcoinData] = useState(null);
+
+  useEffect(() => {
+    const fetchBitcoinData = async () => {
+      try {
+        const response = await fetch(
+          'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,inr&include_24hr_change=true'
+        );
+        if (!response.ok) {
+          throw new Error('Failed to fetch Bitcoin data');
+        }
+        const data = await response.json();
+        setBitcoinData(data.bitcoin);
+      } catch (error) {
+        console.error('Error fetching Bitcoin data:', error);
+      }
+    };
+
+    fetchBitcoinData();
+  }, []);
+
+
   return (
     <div className='coin'>
       <div className='items'>
@@ -16,7 +40,33 @@ const Coin = () => {
           </div>
         </div>
       </div>
-      <div className='chart'>Chart</div>
+      <div>
+        {bitcoinData && (
+          <div className='total'>
+  <div className='Price'>
+    <p className='dollar'>${bitcoinData.usd}</p>
+    <p className={`change ${bitcoinData.usd_24h_change >= 0 ? 'positive' : 'negative'}`}>{bitcoinData.usd_24h_change}%</p><span className='hour'>(24H)</span>
+  </div>
+  
+    <p className='inr'>₹{bitcoinData.inr}</p>
+  
+</div>
+
+        )}
+      </div>
+      <div className='chart'>
+        <TradingViewWidget
+          symbol='COINBASE:BTCUSD'
+          interval='D'
+          timezone='Etc/UTC'
+          locale='en'
+          autosize
+          hide_side_toolbar={false}
+          enable_publishing={false}
+          allow_symbol_change={true}
+          container_id='tv_chart_container'
+        />
+      </div>
     </div>
   );
 };
